48a2bd46bdd68a222f1fcbcd48d0e978a94a38d2
vaishnavk09 <vaishnavk9420@gmail.com>
2025-11-14 23:19:51 +0530

    backend setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const socket= require("socket.io")
 const http=require("http")
 const {Chess}=require("chess.js")
 const path=require("path")
+const { log } = require("console")
 const app=express()
 const server =http.createServer(app)
 
@@ -24,16 +25,53 @@ app.get('/',(req,res)=>{
 })
 
 io.on('connection',(Uniqsocket)=>{
-    console.log("New user connected: "+Uniqsocket.id)
-   
-    Uniqsocket.on('churan',()=>{
-       io.emit('churan papdi')
+    console.log("New user connected: "+Uniqsocket.id)})
 
+    if(!players.white){
+        players.white=Uniqsocket.id
+        Uniqsocket.emit('playerColor','W')
+    }
+    else if(!players.black){
+        players.black=Uniqsocket.id
+        Uniqsocket.emit('playerColor','B')
+    }
+    else{
+        Uniqsocket.emit('playerColor','Spectator')  
+    }
+   
+    Uniqsocket.on("disconnect",()=>{
+        console.log("User disconnected: "+Uniqsocket.id)
+        if(players.white===Uniqsocket.id){
+            delete players.white
+        }
+        else if(players.black===Uniqsocket.id){
+            delete players.black
+        }
     })
 
-})
+    Uniqsocket.on("move",(move)=>{
+        try{
+            if(chess.turn()=='W' && Uniqsocket.id!=players.white)return
+            if(chess.turn()=='B' && Uniqsocket.id!=players.black)return
 
-
+            const result=chess.move(move)
+            if(result)
+            {
+                currentPlayer=chess.turn()
+                io.emit("move",move)
+                io.emit("boardState",chess.fen())
+            }else{
+                console.log("Invalid Move :",move)
+                Uniqsocket.emit("invalidMove",move)
+                
+            }
+        }
+        catch(err){}
+    })
+    
+ 
+ 
+   
 
 server.listen(3000,()=>{
     console.log("Server is running on  http://localhost:3000")
