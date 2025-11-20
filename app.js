f10f6d0314103bd8ccc78737241dd96d6685c9d1
vaishnavk09 <vaishnavk9420@gmail.com>
2025-11-20 18:04:29 +0530

    deployment setup

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ io.on('connection',(Uniqsocket)=>{
  
    
 
-server.listen(3000,()=>{
-    console.log("Server is running on  http://localhost:3000")
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
